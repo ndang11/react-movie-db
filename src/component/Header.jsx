f9ce1d3a6563257203ea4f634262b77ca566a1a9
Ndang11 <royaltyhopsin@gmail.com>
2025-02-16 19:13:51 +0100

    header styling22

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,35 +15,35 @@ export const Header = () => {
                         </ul>
                     </div>
                     <div className="seacrh">
-                        <input type='text' placeholder='Search Movies, Series...'/>
+                        <input type='text' placeholder='Search Movies, Series...' />
                         <img src="/images/profile pic.png" alt="" />
                     </div>
                 </div>
             </div>
-                <section>
-                    <div className="title">
-                        <img src='/images/Spiderman.png' alt="" />
-                        <div className="btn">
-                            <button className="btn22">CBFC:U/A</button>
-                            <button>Action.</button>
-                            <button>Adventure.</button>
-                            <button>2h28m</button>
-                        </div>
-                        <p>When a spell goes wrong, dangerous foes from other<br>
-                        </br> worlds start to appear, forcing Peter to discover what it<br>
+            <section className="sub">
+                <div className="title">
+                    <img src='/images/Spiderman.png' alt="" />
+                    <div className="detail">
+                        <span>CBFC:U/A</span>
+                        <span>Action.</span>
+                        <span>Adventure.</span>
+                        <span>2h 28m</span>
+                    </div>
+                    <p>When a spell goes wrong, dangerous foes from other<br>
+                    </br> worlds start to appear, forcing Peter to discover what it<br>
                         </br> truly means to be Spider-Man.</p>
-                    </div>
-                    <div className="main-btn">
-                        <button>
-                            <img src='/images/playicon.png' alt=""/>
-                            Watch Now
-                        </button>
-                        <button>
-                            <img src='/images/infoicon.png' alt=""/>
-                            More Info
-                        </button>
-                    </div>
-                </section>
+                </div>
+                <div className="main-btn">
+                    <button>
+                        <img src='/images/playicon.png' alt="" />
+                        Watch Now
+                    </button>
+                    <button>
+                        <img src='/images/infoicon.png' alt="" />
+                        More Info
+                    </button>
+                </div>
+            </section>
         </div>
     )
 }
