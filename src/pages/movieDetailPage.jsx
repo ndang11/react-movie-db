ab16d2098a9c6b92c4411c3f7a4f51e3e05f5c6f
Ndang11 <royaltyhopsin@gmail.com>
2025-02-24 11:51:39 +0100

    working on styling detail page

--- src/pages/movieDetailPage.jsx
+++ src/pages/movieDetailPage.jsx
@@ -1,39 +1,39 @@
-
-import Footer from '../component/Footer.jsx';
-import { useContext, useState}  from 'react'
-import { MovieContext } from '../context/Context.jsx';
-import { useNavigate, useParams } from 'react-router-dom'; 
-import Action from '../component/Action.jsx';
-import SimilarMovies from '../component/similarMovies.jsx';
-
+import Footer from "../component/Footer.jsx";
+import { useContext, useState } from "react";
+import { MovieContext } from "../context/Context.jsx";
+import { useNavigate, useParams } from "react-router-dom";
+import Popular from "../component/Popular.jsx";
+import SimilarMovies from "../component/similarMovies.jsx";
+import { HeroSectionDetail } from "../component/HeroSectionDetail.jsx";
+import Drama from "../component/Drama.jsx";
 
 export default function MovieDetailPage() {
-  const[cast, setCast] =  useState([]);
-  const{id} = useParams();
- const { setSelectedMovie } = useContext(MovieContext);  
-    const navigate = useNavigate();
+  const [cast, setCast] = useState([]);
+  const { id } = useParams();
+  const { setSelectedMovie } = useContext(MovieContext);
+  const navigate = useNavigate();
 
-    const { selectedMovie } = useContext(MovieContext);
-  
-   
+  const { selectedMovie } = useContext(MovieContext);
 
-    const handleNavigation = (movie) => {
-        setSelectedMovie(movie);
-        navigate(`/detail/${movie.id}`);
-    };
+  const handleNavigation = (movie) => {
+    setSelectedMovie(movie);
+    navigate(`/detail/${movie.id}`);
+  };
 
-    return (<>
-      
-        <div style={{
-            backgroundImage: selectedMovie?.poster_path
-                ? `url(https://image.tmdb.org/t/p/w200${selectedMovie.poster_path})`
-                : 'url(default-image.jpg)'
-        }}>
-           
-            <SimilarMovies redirectTo={handleNavigation}/>
-            <Action/>
-            <Footer />
-        </div></>
-    );
+  return (
+    <>
+      <HeroSectionDetail />
+      <div
+        style={{
+          backgroundImage: selectedMovie?.poster_path
+            ? `url(https://image.tmdb.org/t/p/w200${selectedMovie.poster_path})`
+            : "url(default-image.jpg)",
+        }}
+      >
+        <Drama/>
+        <Popular redirectTo={handleNavigation} />
+        <Footer />
+      </div>
+    </>
+  );
 }
-
